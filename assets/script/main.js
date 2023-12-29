54767049e76e5c478f3ea65c011ea3a272186e4d
Pratik Trivedi <pratiktrivedi719@gmail.com>
2023-12-29 11:44:03 +0530

    First Commit

---
+++ assets/script/main.js
@@ -0,0 +1,161 @@
+// Smooth Scrolling
+// function locomotiveAnimation() {
+//   gsap.registerPlugin(ScrollTrigger);
+
+//   // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+//   const locoScroll = new LocomotiveScroll({
+//     el: document.querySelector("#main"),
+//     smooth: true,
+//   });
+//   // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+//   locoScroll.on("scroll", ScrollTrigger.update);
+
+//   // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+//   ScrollTrigger.scrollerProxy("#main", {
+//     scrollTop(value) {
+//       return arguments.length
+//         ? locoScroll.scrollTo(value, 0, 0)
+//         : locoScroll.scroll.instance.scroll.y;
+//     }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+//     getBoundingClientRect() {
+//       return {
+//         top: 0,
+//         left: 0,
+//         width: window.innerWidth,
+//         height: window.innerHeight,
+//       };
+//     },
+//     // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+//     pinType: document.querySelector("#main").style.transform
+//       ? "transform"
+//       : "fixed",
+//   });
+
+//   // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+//   ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+//   // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+//   ScrollTrigger.refresh();
+// }
+
+// locomotiveAnimation();
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+});
+
+// Typewriter Animation
+
+var typed = new Typed(".auto-type", {
+  strings: ["Web Developement.", "UI / UX.", "Discord Server Developement"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true,
+});
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden", ".hidden-1");
+hiddenElements.forEach((el) => observer.observe(el));
+// hsl(210,100%,80%)
+
+// Text Animation
+
+const elts = {
+  text1: document.getElementById("text1"),
+  text2: document.getElementById("text2"),
+};
+
+const texts = ["My", "Games:)"];
+
+const morphTime = 1;
+const cooldownTime = 0.1;
+
+let textIndex = texts.length - 1;
+let time = new Date();
+let morph = 0;
+let cooldown = cooldownTime;
+
+elts.text1.textContent = texts[textIndex % texts.length];
+elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+
+function doMorph() {
+  morph -= cooldown;
+  cooldown = 0;
+
+  let fraction = morph / morphTime;
+
+  if (fraction > 1) {
+    cooldown = cooldownTime;
+    fraction = 1;
+  }
+
+  setMorph(fraction);
+}
+
+function setMorph(fraction) {
+  elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+  fraction = 1 - fraction;
+  elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+  elts.text1.textContent = texts[textIndex % texts.length];
+  elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+}
+
+function doCooldown() {
+  morph = 0;
+
+  elts.text1.style.filter = "";
+  elts.text1.style.opacity = "0%";
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  let newTime = new Date();
+  let shouldIncrementIndex = cooldown > 0;
+  let dt = (newTime - time) / 2000;
+  time = newTime;
+
+  cooldown -= dt;
+
+  if (cooldown <= 0) {
+    if (shouldIncrementIndex) {
+      textIndex++;
+    }
+
+    doMorph();
+  } else {
+    doCooldown();
+  }
+}
+
+animate();
+
+// Navbar changes color onScroll
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    document.getElementById("myTopnav").style.backgroundColor = "#1d1d1d";
+  } else {
+    document.getElementById("myTopnav").style.backgroundColor = "#1d1d1d00";
+  }
+}
